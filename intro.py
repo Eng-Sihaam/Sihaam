1d9d7f7aa8176c620ccb9703d06516019b6583c4
Ina Mohamed <sihaammoha2223@gmail.com>
2024-12-23 10:09:50 +0300

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,6 @@
+# salary=int(input("soo gali mushaarkaga walaley: "))
+# if salary >= 20000:
+#     years_on_the_job = int(input("soo gali sanadaha aad shaqeysay walaley: "))
+#     if years_on_the_job >= 3:
+#         print("waa u qalantaa deyn in lagu siiyo")
+
